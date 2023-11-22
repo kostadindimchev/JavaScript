1b6011571a3ce25b3b51b435f969443d4acb8f8e
kostadindimchev <143182432+kostadindimchev@users.noreply.github.com>
2023-11-22 15:32:35 +0200

    Delete lecture 1 directory

--- lecture 1/petShop.js
+++
@@ -1,8 +0,0 @@
-function petShop(input) {
-    let dogsFood = Number(input[0]) * 2.5
-    let catsFood = Number(input[1]) * 4
-    let sum = dogsFood + catsFood
-
-    console.log(`${sum}.lv`);
-}
-petShop([5, 4]);
--- lecture 1/projectsCreation.js
+++
@@ -1,8 +0,0 @@
-function projectsCreaton(input) {
-    let name = (input[0])
-    let count = (input[1])
-    let hours = (input[1]) * 3
-
-    console.log(`The architect ${name} will need ${hours} hours to complete ${count} project/s.`);
-}
-projectsCreaton(["George", 5])
