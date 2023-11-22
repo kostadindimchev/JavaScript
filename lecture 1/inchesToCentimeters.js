88fe6e074ee1ec6daf3b99e5dea39b9195745eb2
kostadindimchev <143182432+kostadindimchev@users.noreply.github.com>
2023-11-22 15:16:28 +0200

    Add files via upload

---
+++ lecture 1/inchesToCentimeters.js
@@ -0,0 +1,10 @@
+function inchesToCentimeters(input) {
+    let inch = 2.54;
+    let num = Number(input[0]);
+    let result = inch * num
+
+    console.log(result);
+    
+}
+inchesToCentimeters(["5"])
+
