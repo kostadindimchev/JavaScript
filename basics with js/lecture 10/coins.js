ebf34584d65ba61d2729103f7573dda83f2d13b5
KostadinDimchev <kostadindimchev17@gmail.com>
2023-12-03 17:50:51 +0200

    Add files via upload

---
+++ basics with js/lecture 10/coins.js
@@ -0,0 +1,30 @@
+function coins(input) {
+    let change = Number(input[0]);
+    let changeInStotinki = Math.round(change * 100)
+    let coinsCount = 0
+
+    while(changeInStotinki !== 0) {
+        if(changeInStotinki >= 200) {
+            changeInStotinki -= 200
+        }else if(changeInStotinki >= 100) {
+            changeInStotinki -= 100
+        }else if(changeInStotinki >= 50) {
+            changeInStotinki -= 50
+        }else if(changeInStotinki >= 20) {
+            changeInStotinki -= 20
+        }else if(changeInStotinki >= 10) {
+            changeInStotinki -= 10
+        }else if(changeInStotinki >= 5) {
+            changeInStotinki -= 5
+        }else if(changeInStotinki >= 2) {
+            changeInStotinki -= 2
+        }else if(changeInStotinki >= 1) {
+            changeInStotinki -= 1
+        }
+
+        coinsCount++
+    }
+    console.log(coinsCount);
+}
+
+coins(["0.56"])
