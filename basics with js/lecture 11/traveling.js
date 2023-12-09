f71d4785c8fc9c58bec58f3528327ba872700920
KostadinDimchev <kostadindimchev17@gmail.com>
2023-12-09 20:34:37 +0200

    Add files via upload

---
+++ basics with js/lecture 11/traveling.js
@@ -0,0 +1,38 @@
+function traveling(input) {
+    index = 0
+    let row = input[index];
+
+    while(row !== "End") {
+        let destination = input[index]
+        index++
+        let budget = Number(input[index])
+        let sum = 0
+
+        while(sum < budget){
+            index++
+            let curSum = Number(input[index])
+            sum += curSum
+        }
+
+        console.log(`Going to ${destination}!`);
+       
+        index++
+         row = input[index]
+    }
+
+}
+traveling(["Greece",
+"1000",
+"200",
+"200",
+"300",
+"100",
+"150",
+"240",
+"Spain",
+"1200",
+"300",
+"500",
+"193",
+"423",
+"End"])
