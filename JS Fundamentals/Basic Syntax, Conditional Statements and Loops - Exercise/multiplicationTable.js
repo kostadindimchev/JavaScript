c512cc275ccc078aa85a778e229b6f9196bfba72
KostadinDimchev <kostadindimchev17@gmail.com>
2024-01-11 23:10:42 +0200

    Add files via upload

---
+++ JS Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/multiplicationTable.js
@@ -0,0 +1,10 @@
+function multiplicationTable(num) {
+    for(let i = 1; i <= 10; i++) {
+        let result = num * i
+        console.log(`${num} X ${i} = ${result}`);
+    }
+
+}
+
+multiplicationTable(5);
+multiplicationTable(2);
