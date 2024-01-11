3ed47499a732e14f118fe4922c0032fa3a89c958
KostadinDimchev <kostadindimchev17@gmail.com>
2024-01-11 23:11:17 +0200

    Add files via upload

---
+++ JS Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/rounding.js
@@ -0,0 +1,13 @@
+function rounding(num, precision) {
+    num = Number(num);
+    precision = Number(precision);
+
+    if(precision > 15) {
+        precision = 15
+    }
+
+    console.log(`${parseFloat(num.toFixed(precision))}`);
+}
+
+rounding(3.1415926535897932384626433832795,2);
+rounding(10.5,3);
