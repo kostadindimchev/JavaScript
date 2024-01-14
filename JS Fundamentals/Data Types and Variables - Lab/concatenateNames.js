33341784129cabfe0871b79f9304e8f8e6bacb90
KostadinDimchev <kostadindimchev17@gmail.com>
2024-01-14 14:43:08 +0200

    Add files via upload

---
+++ JS Fundamentals/Data Types and Variables - Lab/concatenateNames.js
@@ -0,0 +1,7 @@
+function concatenateNames(nameOne, nameTwo, dlimeter) {
+    console.log(nameOne + dlimeter + nameTwo);
+}
+
+concatenateNames('John', 'Smith', '->');
+concatenateNames('Jan', 'White', '<->');
+concatenateNames('Linda', 'Terry', '=>');
