f0bef0548ad4bb927d8d499a7c68aa5d52c8de88
KostadinDimchev <kostadindimchev17@gmail.com>
2024-01-20 19:30:53 +0200

    Add files via upload

---
+++ JS Fundamentals/Data Types and Variables - Exercise/calculator.js
@@ -0,0 +1,22 @@
+function calculator(number, operator, anotherNumber) {
+  let result = 0;
+  switch (operator) {
+    case "+":
+      result = number + anotherNumber;
+      break;
+    case "-":
+      result = number - anotherNumber;
+      break;
+    case "*":
+      result = number * anotherNumber;
+      break;
+    case "/":
+      result = number / anotherNumber;
+      break;
+  }
+
+  console.log(result.toFixed(2));
+}
+
+calculator(5, "+", 10);
+calculator(25.5, "-", 3);
