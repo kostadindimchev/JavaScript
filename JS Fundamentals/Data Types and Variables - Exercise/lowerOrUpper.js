ea7611523b3fab9c97af34aa2c5a5f8c1f37b137
KostadinDimchev <kostadindimchev17@gmail.com>
2024-01-21 22:39:28 +0200

    Add files via upload

---
+++ JS Fundamentals/Data Types and Variables - Exercise/lowerOrUpper.js
@@ -0,0 +1,7 @@
+function lowerOrUpper(char) {
+  let charToUpper = char.toUpperCase();
+  console.log(char === charToUpper ? "upper-case" : "lower-case");
+}
+
+lowerOrUpper("L");
+lowerOrUpper("f");
