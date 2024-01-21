d060178900c565d7e12eced4c5a16cfe01d79dac
KostadinDimchev <kostadindimchev17@gmail.com>
2024-01-21 22:38:27 +0200

    Add files via upload

---
+++ JS Fundamentals/Data Types and Variables - Exercise/charsToString.js
@@ -0,0 +1,7 @@
+function charsToString(firstChar, secondChar, thirdChar) {
+  console.log(`${firstChar}${secondChar}${thirdChar}`);
+}
+
+charsToString("a", "b", "c");
+charsToString("%", "2", "o");
+charsToString("1", "5", "p");
