e8f111b63869bb846ab2478295aeb93e2dd196ad
KostadinDimchev <kostadindimchev17@gmail.com>
2024-01-26 23:20:03 +0200

    Add files via upload

---
+++ JS Fundamentals/Arrays - Lab/sumFirstAndLastArrayElments.js
@@ -0,0 +1,8 @@
+function sumFirstAndLastArrayElements(array) {
+  sum = array[0] + array[array.length - 1];
+  console.log(sum);
+}
+
+sumFirstAndLastArrayElements([20, 30, 40]);
+sumFirstAndLastArrayElements([10, 17, 22, 33]);
+sumFirstAndLastArrayElements([11, 58, 69]);
