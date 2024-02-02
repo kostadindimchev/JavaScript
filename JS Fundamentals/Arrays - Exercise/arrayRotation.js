8b11306b3f90a485075f67f5bbdd3a01cd9a0dd3
KostadinDimchev <kostadindimchev17@gmail.com>
2024-02-02 22:24:39 +0200

    Add files via upload

---
+++ JS Fundamentals/Arrays - Exercise/arrayRotation.js
@@ -0,0 +1,12 @@
+function arrayRotation(arr, rotations) {
+  for (i = 1; i <= rotations; i++) {
+    let save = arr.shift();
+    arr.push(save);
+  }
+
+  console.log(arr.join(" "));
+}
+
+arrayRotation([51, 47, 32, 61, 21], 2);
+arrayRotation([32, 21, 61, 1], 4);
+arrayRotation([2, 4, 15, 31], 5);
