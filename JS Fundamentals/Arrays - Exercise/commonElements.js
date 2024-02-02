a61c42ae30e6f5699cc088d6d7879b0df0afa75c
KostadinDimchev <kostadindimchev17@gmail.com>
2024-02-02 22:24:55 +0200

    Add files via upload

---
+++ JS Fundamentals/Arrays - Exercise/commonElements.js
@@ -0,0 +1,18 @@
+function commonElements(arrOne, arrTwo) {
+  for (let i = 0; i < arrOne.length; i++) {
+    for (let j = 0; j < arrTwo.length; j++) {
+      if (arrOne[i] === arrTwo[j]) {
+        console.log(arrOne[i]);
+      }
+    }
+  }
+}
+
+commonElements(
+  ["Hey", "hello", 2, 4, "Peter", "e"],
+  ["Petar", 10, "hey", 4, "hello", "2"]
+);
+commonElements(
+  ["S", "o", "f", "t", "U", "n", "i", " "],
+  ["s", "o", "c", "i", "a", "l"]
+);
