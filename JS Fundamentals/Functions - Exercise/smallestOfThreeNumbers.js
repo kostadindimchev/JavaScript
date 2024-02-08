967b8e3d7e212fe799a40842d9d12eba9ae1bf92
KostadinDimchev <kostadindimchev17@gmail.com>
2024-02-08 13:17:02 +0200

    Add files via upload

---
+++ JS Fundamentals/Functions - Exercise/smallestOfThreeNumbers.js
@@ -0,0 +1,9 @@
+function smallestOfThreeNumbers(numOne, numTwo, numThree) {
+    let smallest = Math.min(numOne, numTwo, numThree)
+    console.log(smallest);
+}
+
+smallestOfThreeNumbers(2, 5, 3);
+smallestOfThreeNumbers(600, 342, 123);
+smallestOfThreeNumbers(25, 21, 4);
+smallestOfThreeNumbers(2, 2, 2);
