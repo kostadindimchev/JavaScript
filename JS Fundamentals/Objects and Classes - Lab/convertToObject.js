0f781880b4d03fa5bd5b4d218ca987822ff3eb6f
KostadinDimchev <kostadindimchev17@gmail.com>
2024-02-26 22:50:55 +0200

    Add files via upload

---
+++ JS Fundamentals/Objects and Classes - Lab/convertToObject.js
@@ -0,0 +1,10 @@
+function convertToObject(json) {
+    let obj = JSON.parse(json);
+    
+    for (let key of Object.keys(obj)) {
+        console.log(`${key}: ${obj[key]}`);
+    }  
+}
+
+convertToObject('{"name": "George", "age": 40, "town": "Sofia"}');
+convertToObject('{"name": "Peter", "age": 35, "town": "Plovdiv"}');
