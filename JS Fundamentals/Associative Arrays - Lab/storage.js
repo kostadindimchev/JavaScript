61a08d32d5557f3658730ea4ef894b5f0063ba84
KostadinDimchev <kostadindimchev17@gmail.com>
2024-03-01 16:28:04 +0200

    Add files via upload

---
+++ JS Fundamentals/Associative Arrays - Lab/storage.js
@@ -0,0 +1,27 @@
+function storage(input) {
+    let result = new Map();
+
+    for (let line of input) {
+        let [product, qty] = line.split(' ');
+        qty = Number(qty);
+        if(result.has(product)) {
+            let existed = result.get(product)
+            result.set(product, qty + existed)
+        }else{
+            result.set(product, qty)
+        }
+    }
+
+    for (let [product, qty] of result) {
+        console.log(product, '->', qty);
+    }
+}
+
+storage(['tomatoes 10',
+'coffee 5',
+'olives 100',
+'coffee 40']);
+storage(['apple 50',
+'apple 61',
+'coffee 115',
+'coffee 40']);
