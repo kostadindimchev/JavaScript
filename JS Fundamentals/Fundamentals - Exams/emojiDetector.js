98bca267bca5fce837a4f4273d8f4c4d7869b9a4
KostadinDimchev <kostadindimchev17@gmail.com>
2024-03-31 22:58:34 +0300

    Add files via upload

---
+++ JS Fundamentals/Fundamentals - Exams/emojiDetector.js
@@ -0,0 +1,5 @@
+"{countOfAllEmojis} emojis found in the text. The cool ones are:
+
+emojiDetector(["In the Sofia Zoo there are 311 animals in total! ::Smiley:: This includes 3 **Tigers**, 1 ::Elephant:, 12 **Monk3ys**, a **Gorilla::, 5 ::fox:es: and 21 different types of :Snak::Es::. ::Mooning:: **Shy**"]);
+emojiDetector(["5, 4, 3, 2, 1, go! The 1-th consecutive banana-eating contest has begun! ::Joy:: **Banana** ::Wink:: **Vali** ::valid_emoji::"]);
+emojiDetector(["It is a long established fact that 1 a reader will be distracted by 9 the readable content of a page when looking at its layout. The point of using ::LoremIpsum:: is that it has a more-or-less normal 3 distribution of 8 letters, as opposed to using 'Content here, content 99 here', making it look like readable **English**."]);
