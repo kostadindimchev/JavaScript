1003fcbb745c99deb3d74797497edd547fa4a034
KostadinDimchev <kostadindimchev17@gmail.com>
2024-04-10 17:41:44 +0300

    Delete JS Advanced/Syntax, Functions and Statements - Lab directory

--- JS Advanced/Syntax, Functions and Statements - Lab/Syntax, Functions and Statements - Exercise/fruit.js
+++
@@ -1,9 +0,0 @@
-function fruit(fruit, weight, pricePerKg) {
-    let weightInKg = weight / 1000;
-    let price = pricePerKg * weightInKg
-
-    console.log(`I need $${price.toFixed(2)} to buy ${weightInKg.toFixed(2)} kilograms ${fruit}.`);
-}
-
-fruit('orange', 2500, 1.80);
-fruit('apple', 1563, 2.35);
