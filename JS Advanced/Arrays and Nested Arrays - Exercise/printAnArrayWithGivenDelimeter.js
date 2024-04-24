a73a8581a5af34d4c15eb5afb45b6c2ce66ceca0
KostadinDimchev <kostadindimchev17@gmail.com>
2024-04-24 19:41:47 +0300

    Add files via upload

---
+++ JS Advanced/Arrays and Nested Arrays - Exercise/printAnArrayWithGivenDelimeter.js
@@ -0,0 +1,17 @@
+function printAnArrayWithGivenDelimeter(array, delimeter) {
+    console.log(array.join(delimeter));
+}
+
+printAnArrayWithGivenDelimeter(['One', 
+'Two', 
+'Three', 
+'Four', 
+'Five'], 
+'-');
+printAnArrayWithGivenDelimeter(['How about no?', 
+'I',
+'will', 
+'not', 
+'do', 
+'it!'], 
+'_');
