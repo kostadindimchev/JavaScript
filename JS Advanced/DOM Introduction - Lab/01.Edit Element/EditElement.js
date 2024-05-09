f62eb2057ebd154c086306189ff80222d9d8f476
KostadinDimchev <kostadindimchev17@gmail.com>
2024-05-09 12:17:14 +0300

    Delete JS Advanced/DOM Introduction - Lab/01.Edit Element/EditElement.js

--- JS Advanced/DOM Introduction - Lab/01.Edit Element/EditElement.js
+++
@@ -1,5 +0,0 @@
-function editElement(reference, match, replacer) {
-    while(reference.textContent.includes(match)){
-        reference.textContent = reference.textContent.replace(match, replacer)
-    }
-}
