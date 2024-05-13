6d5efa5c0666293c2e1459c15d30894054e1a417
KostadinDimchev <kostadindimchev17@gmail.com>
2024-05-13 21:30:29 +0300

    Add files via upload

---
+++ JS Advanced/DOM Introduction - Lab/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    let text = document.getElementById('text');
+    text.style.display = 'inline'
+
+    let readMore = document.getElementById('more');
+    readMore.style.display = 'none'
+}
