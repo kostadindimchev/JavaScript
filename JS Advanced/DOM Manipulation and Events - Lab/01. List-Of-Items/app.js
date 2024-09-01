2cf4742ce087238a0a3ef5863775ca139792dbd6
KostadinDimchev <kostadindimchev17@gmail.com>
2024-09-01 19:53:53 +0300

    Delete JS Advanced/DOM Manipulation and Events - Lab/01. List-Of-Items directory

--- JS Advanced/DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
+++
@@ -1,9 +0,0 @@
-function addItem() {
-    let inputElement = document.getElementById('newItemText');
-    let liElement = document.getElementById('items')
-
-    let newElement = document.createElement('li');
-    newElement.textContent = inputElement.value
-    liElement.appendChild(newElement);
-    inputElement.value = '';
-}
